2e3180ace3cfdd1b4cff991be12dcde417366f26
amidelu <delowar_gtc@yahoo.com>
2019-05-09 17:31:40 +0600

    upto bottom navigation drawer

--- app/src/main/java/com/amidelu/personaldemo/MainActivity.java
+++ app/src/main/java/com/amidelu/personaldemo/MainActivity.java
@@ -1,33 +1,40 @@
 package com.amidelu.personaldemo;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Switch;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,BottomNavigationView.OnNavigationItemSelectedListener {
     CardView socialCard, weatherCard, restaurantCard, carCard;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Defining ID with CardView
+//        Defining ID for CardView and Bottom Navigation
         socialCard = findViewById(R.id.socialCardViewId);
         weatherCard = findViewById(R.id.weatherCardViewId);
         restaurantCard = findViewById(R.id.restaurantCardViewId);
         carCard = findViewById(R.id.carCardViewId);
 
-//        Implementing onclick on every card
+        bottomNavigationView = findViewById(R.id.navigationBottomId);
+
+//        Implementing onclick on every card and bottom navigation
         socialCard.setOnClickListener(this);
         weatherCard.setOnClickListener(this);
         restaurantCard.setOnClickListener(this);
         carCard.setOnClickListener(this);
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
     }
 
 //    On Click method implemented for every CardView
@@ -55,6 +62,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         default:break;
         }
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        int id = menuItem.getItemId();
+
+        switch (id) {
+
+            case R.id.myProfileBottomId:
+                startActivity(new Intent(this, MyProfile.class));
+                break;
+
+            case R.id.dashboardBottomId:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+
+            case R.id.settingsBottomId:
+                Toast.makeText(MainActivity.this, "Settings Bottom Navigation clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return false;
     }
 }
